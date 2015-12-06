534ab85196b0f3bb863662d25db5b861a8fb1210
Wayne Cheng <waynethecheng@gmail.com>
2015-12-06 01:29:30 +0000

    Update naming

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var usersController = require('../controllers/UsersController');
+var UsersController = require('../controllers/userscontroller');
 
 /* GET users listing. */
-router.get('/userlist', usersController.getUserList);
-router.post('/adduser', usersController.addUser);
-router.delete('/deleteuser/:id', usersController.deleteUser);
+router.get('/userlist', UsersController.getUserList);
+router.post('/adduser', UsersController.addUser);
+router.delete('/deleteuser/:id', UsersController.deleteUser);
 
 module.exports = router;
