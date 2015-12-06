32ef3c743df20b363d5662e6c00d9d1a1435a57a
Wayne Cheng <waynethecheng@gmail.com>
2015-12-06 01:31:43 +0000

    Skeleton for userscontroller

---
+++ controllers/userscontroller.js
@@ -0,0 +1,15 @@
+var UsersController = UsersController || {};
+
+UsersController.getUserList = function(req, res){
+
+};
+
+UsersController.addUser = function(req, res){
+
+};
+
+UsersController.deleteUser = function(req, res){
+
+};
+
+module.exports = UsersController;
