29563f8ee6bc8f31b1cc679f97b3134a295e902b
Wayne Cheng <waynethecheng@gmail.com>
2015-12-11 05:53:35 +0000

    Update model to include child schema that includes games from steam

--- models/user.js
+++ models/user.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose');
 
-var schema = new mongoose.Schema({ steamID: 'number', avatarURL: 'string' });
-var User = mongoose.model('User', schema);
+var gameSchema = new mongoose.Schema({ gameName: 'string', recentPlaytime: 'number', imgURL: 'string'});
+var userSchema = new mongoose.Schema({ steamID: 'number', avatarURL: 'string', games: [gameSchema] });
+
+var User = mongoose.model('User', userSchema);
 
 // Test creation
-User.create({ steamID: 12345, avatarURL: 'http://www.example.com' }, function (err) {
+User.create({ steamID: 12345, avatarURL: 'http://www.example.com', games: [
+    {gameName: 'Farcry 3', recentPlayTime: 50, imgURL: 'http://www.example.com/img.jpg'},
+    {gameName: 'Fallout 4', recentPlayTime: 0, imgURL: 'http://www.example.com/img.jpg'}
+] }, function (err) {
     if (err) return handleError(err);
     console.log('Test user created');
 });
