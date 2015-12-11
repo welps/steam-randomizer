fae0ab3b68fc5046d5b72de239db59a6db4049e1
Wayne Cheng <waynethecheng@gmail.com>
2015-12-11 12:32:47 +0000

    User creation logic

--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -1,10 +1,22 @@
 var UsersController = UsersController || {};
-
-UsersController.getUserList = function(req, res){
-
-};
+var UserModel  = require('../models/user');
 
 UsersController.addUser = function(req, res){
+   UserModel.count({ steamID: req.body.steamID }, function (err, count){
+       if (count === 0){
+           UserModel.create(req.body, function (err) {
+               if (err) console.log(err);
+           });
+       }
+       else {
+           UserModel.findOneAndUpdate({ steamID: req.body.steamID}, req.body, function(err){
+               if (err) console.log(err);
+           });
+       }
+   });
+};
+
+UsersController.getUser = function(req, res){
 
 };
 
