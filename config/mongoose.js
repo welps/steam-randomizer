768ede064fd1b606f07c8eba7517d65531cdd290
Wayne Cheng <waynethecheng@gmail.com>
2015-12-13 04:37:45 +0000

    Kill node if db connection fails

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,7 +5,11 @@ mongoose.connect(ENV.DB_LOCATION);
 
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', function(err){
+    console.log('Connection Error: ' + err);
+    process.exit(1);
+});
+
 db.once('open', function (callback) {
     console.log('DB successfully connected.');
 });
