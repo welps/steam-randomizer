875d22ceb7104d568463f68dd0c4c0cc3edca688
Wayne Cheng <waynethecheng@gmail.com>
2015-12-16 04:22:11 +0000

    We actually need the appip to pull the game image

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var gameSchema = new mongoose.Schema({ name: 'string', playtime_2weeks: 'number', img_logo_url: 'string'});
+var gameSchema = new mongoose.Schema({ appid: 'number', name: 'string', playtime_2weeks: 'number', img_logo_url: 'string'});
 var userSchema = new mongoose.Schema({ steamID: 'number', displayName: 'string', avatarURL: 'string', games: [gameSchema] });
 
 var User = mongoose.model('User', userSchema);
