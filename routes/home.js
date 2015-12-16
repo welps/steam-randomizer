16e7ad74ed7a813d0ca7dd6afb90f73f3965d619
Wayne Cheng <waynethecheng@gmail.com>
2015-12-16 04:25:08 +0000

    Remove redundant comments

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var HomeController = require('../controllers/homecontroller');
 
-/* GET home page. */
 router.get('/', HomeController.getHomepage);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var UsersController = require('../controllers/userscontroller');
 
-/* GET users listing. */
 router.get('/user/:id', UsersController.getUser);
 router.get('/randomgame/:id', UsersController.getRandomGame);
 
