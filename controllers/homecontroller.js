14327d2f2e4da1b2310de1137f410e80e98c8d72
Wayne Cheng <waynethecheng@gmail.com>
2015-12-16 04:25:53 +0000

    Home Controller should pass steamId to view

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -2,7 +2,7 @@ var HomeController = HomeController || {};
 
 HomeController.getHomepage = function(req, res){
     if (req.isAuthenticated()){
-        res.render('index', {user: req.user.id});
+        res.render('index', {user: req.user.displayName, steamId: req.user.id});
     }
     else {
         res.render('index');
