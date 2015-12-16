0cbb002a0077c1ad328660f865e82e4845bc6e9a
Wayne Cheng <waynethecheng@gmail.com>
2015-12-16 04:24:36 +0000

    Simple script to pull the game data from endpoint using fed steamid

---
+++ public/js/global.js
@@ -0,0 +1,25 @@
+(function(){
+    $(document).ready(function(){
+        grabGame();
+    });
+
+    function grabGame(){
+        var $steamId;
+
+        if ( ($steamId = $('#steam-id').val()) ){
+            $.ajax({
+                type: 'GET',
+                dataType: 'json',
+                url: 'users/randomgame/' + $steamId,
+                success: function (data){
+                    var gameImage = 'http://media.steampowered.com/steamcommunity/public/images/apps/' + data.appid + '/' + data.img_logo_url + '.jpg';
+                    $('#game p').html('<img src="' + gameImage + '" alt="' + data.name + '"/>');
+                },
+                error: function (error){
+                    $('#game p').text('Please try again.');
+                }
+            });
+        }
+    }
+
+})();
