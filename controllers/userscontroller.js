f7ef2e04d26ba221d9674ddc95b97771f6dace14
Wayne Cheng <waynethecheng@gmail.com>
2015-12-18 19:37:12 +0000

    These methods should belong to the model

--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -1,47 +1,35 @@
 var UsersController = UsersController || {};
-var UserModel  = require('../models/user');
+var UserModel = require('../models/user');
 
-UsersController.addUser = function(req, res){
-   UserModel.count({ steamID: req.body.steamID }, function (err, count){
-       if (count === 0){
-           UserModel.create(req.body, function (err) {
-               if (err) console.log(err);
-           });
-       }
-       else {
-           UsersController.updateUser(req, res);
-       }
-   });
-};
-
-UsersController.getUser = function(req, res){
-    UserModel.findOne({ steamID: req.params.id}, function(err, userData){
-        return res.send(userData);
+UsersController.addUser = function(req, res, next){
+    UserModel.addUser(req.body.steamID, req.body, function (err){
+        if (err) return next(err);
     });
 };
 
-UsersController.updateUser = function(req, res){
-    UserModel.findOneAndUpdate({ steamID: req.body.steamID}, req.body, function(err){
-        if (err) console.log(err);
+UsersController.getUser = function(req, res, next){
+    UserModel.getUser(req.params.id, function(err, userData){
+        if (err) return next(err);
+        res.send(userData);
     });
 };
 
-UsersController.addGames = function(req, res){
-    UserModel.findOneAndUpdate({ steamID: req.body.steamID}, { games: req.body.games} , function(err){
-       if (err) console.log(err);
+UsersController.updateUser = function(req, res, next){
+    UserModel.updateUser(req.body.steamID, req.body, function(err){
+        if (err) return next(err);
     });
 };
 
-UsersController.getRandomGame = function(req, res){
-    UserModel.findOne({ steamID: req.params.id}, function(err, userData){
-        if (userData) {
-            var numGames = userData.games.length;
-            var randomNumber = Math.floor(Math.random() * numGames);
+UsersController.addGames = function(req, res, next){
+    UserModel.addGames(req.body.steamID, req.body.games, function(err){
+        if (err) return next(err);
+    });
+};
 
-            return res.send(userData.games[randomNumber]);
-        }
-
-        return res.send('');
+UsersController.getRandomGame = function(req, res, next){
+    UserModel.getRandomGame(req.params.id, function(err, randomGame){
+        if (err) return next(err);
+        res.send(randomGame);
     });
 };
 
