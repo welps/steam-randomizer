cbebea518ce03bfb188934af2f5e618f94c3c666
welps <waynethecheng@gmail.com>
2015-12-22 00:59:28 +0000

    Let's make SteamID the unique id in the model, reassign steamid property as a virtual, and use updateUser with upsert set to true to maintain uniqueness.

--- models/user.js
+++ models/user.js
@@ -1,42 +1,37 @@
 var mongoose = require('mongoose');
 
 var gameSchema = new mongoose.Schema({ appid: 'number', name: 'string', playtime_2weeks: 'number', img_logo_url: 'string'});
-var userSchema = new mongoose.Schema({ steamID: 'number', displayName: 'string', avatarURL: 'string', games: [gameSchema] });
+var userSchema = new mongoose.Schema({ _id: 'number', displayName: 'string', avatarURL: 'string', games: [gameSchema] });
+
+userSchema.virtual('steamID').get(function(){
+    return this._id;
+});
 
 userSchema.statics.addUser = function(steamId, userData, cb){
-    this.count({ steamID: steamId }, function (err, count){
-        if (count === 0){
-            this.model.create(userData, function (err) {
-                if (err) return cb(err);
-            });
-        }
-        else {
-            this.model.updateUser(steamId, userData, cb);
-        }
-    });
+    this.updateUser(steamId, userData, cb);
 }
 
 userSchema.statics.getUser = function(steamId, cb){
-    this.findOne({ steamID: steamId}, function(err, userData){
+    this.findOne({ _id: steamId}, function(err, userData){
         if (err) return cb(err);
         return cb(null, userData);
     });
 }
 
 userSchema.statics.updateUser = function(steamId, userData, cb){
-    this.findOneAndUpdate({ steamID: steamId}, userData, function(err){
+    this.findOneAndUpdate({ _id: steamId}, userData, {upsert: true}, function(err){
         if (err) return cb(err);
     });
 }
 
 userSchema.statics.addGames = function(steamId, gamesData, cb){
-    this.findOneAndUpdate({ steamID: steamId}, { games: gamesData} , function(err){
+    this.findOneAndUpdate({ _id: steamId}, { games: gamesData} , function(err){
         if (err) return cb(err);
     });
 }
 
 userSchema.statics.getRandomGame = function(steamId, cb){
-    this.findOne({ steamID: steamId}, function(err, userData){
+    this.findOne({ _id: steamId}, function(err, userData){
         if (err) return cb(err);
 
         if (userData) {
