3f9d302a8c6a664fdb7ba8bea4ba4bce69bc12d7
welps <waynethecheng@gmail.com>
2016-01-01 13:39:50 +0000

    pass next

--- controllers/steamcontroller.js
+++ controllers/steamcontroller.js
@@ -5,24 +5,25 @@ var ENV = require('../config/env');
 var steam = require('steam-web');
 var steamService = new steam({apiKey: ENV.API_KEY, format: 'json'});
 
-SteamController.bringInUserData = function(req, res){
+SteamController.bringInUserData = function(req, res, next){
     if (req.user) {
         req.body = {steamID: req.user.id, displayName: req.user.displayName, avatarURL: req.user.photos[2].value};
-        UsersController.addUser(req, res);
-        SteamController.getUsersGames(req, res);
+        UsersController.addUser(req, res, next);
+        SteamController.getUsersGames(req, res, next);
     };
-    res.redirect('/');
 };
 
-SteamController.getUsersGames = function (req, res){
+SteamController.getUsersGames = function (req, res, next){
     steamService.getOwnedGames({
         steamid: req.user.id,
         include_appinfo: 1,
         include_played_free_games: 1,
         callback: function(err,data) {
+            if (err) return next(err);
             req.body.steamID = req.user.id;
             req.body.games = data.response.games;
-            UsersController.addGames(req);
+            UsersController.addGames(req, res, next);
+            res.redirect('/');
         }
     });
 }
