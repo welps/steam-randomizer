f598957128ab959e5e8ea5ca6213002ab5238d52
welps <waynethecheng@gmail.com>
2016-01-05 01:35:28 +0000

    Changed front-end to pull 10 games from api and animate selection of these games.

--- public/js/global.js
+++ public/js/global.js
@@ -1,32 +1,23 @@
 (function(){
+
     $(document).ready(function(){
-        grabGame();
+        getGames();
         $('#getGame').click(function(){
-            grabGame();
+            location.reload();
         });
     });
 
-    function grabGame(){
+    function getGames(){
         var $steamId;
 
         if ( ($steamId = $('#steam-id').val()) ){
             $.ajax({
                 type: 'GET',
                 dataType: 'json',
-                url: 'user/' + $steamId + '/randomgame/',
+                url: 'user/' + $steamId + '/randomgame/10',
                 success: function (data){
-                    var gameId = data.games[0].appid;
-                    var gameName = data.games[0].name;
-                    var gameImageUrl = data.games[0].img_logo_url;
-
-                    var gameImage = 'http://media.steampowered.com/steamcommunity/public/images/apps/' + gameId + '/' + gameImageUrl + '.jpg';
-                    var gameImageHtml = '<img src="' + gameImage + '" alt="' + gameName + '"/>';
-                    var gameLink = '<a href="steam://run/' + gameId + '"></a>';
-
-                    $('#game__image').html(gameLink);
-                    $('#game__image > a').append(gameImageHtml);
-                    $('#game__name').html(gameLink);
-                    $('#game__name > a').append(gameName);
+                    addGamesToPage(data);
+                    pickGame();
                 },
                 error: function (error){
                     $('#game__image').text('Please try again.');
@@ -35,4 +26,49 @@
         }
     }
 
+    function addGamesToPage(gamesJson){
+        var gameId, gameName, gameImageUrl, gameImage, gameImageHtml, gameLink, gameDiv, gamesDiv;
+
+        gamesDiv = '';       
+        for (var i = 0; i < gamesJson.games.length; i++){
+                gameId = gamesJson.games[i].appid;
+                gameName = gamesJson.games[i].name;
+                gameImageUrl = gamesJson.games[i].img_logo_url;
+
+                gameNameHtml = '<p class="game__name">' + gameName + '</p>';
+                gameImage = 'http://media.steampowered.com/steamcommunity/public/images/apps/' + gameId + '/' + gameImageUrl + '.jpg';
+                gameImageHtml = '<p class="game__image"><img src="' + gameImage + '" alt="' + gameName + '"/></p>';
+                gameLinkHtml = '<a href="steam://run/' + gameId + '">' + gameNameHtml + '</a>';
+            
+                gameDiv = '<div class="game">' + gameImageHtml + gameLinkHtml + '</div>';
+                gamesDiv += gameDiv;
+            }
+
+            $('#games').html(gamesDiv);
+    }
+
+    function pickGame(){
+        $('.game').hide();
+        var timesToRun = Math.floor(Math.random() * (50)) + 5;
+
+        showNextGame($('.game').first(), timesToRun);
+    }
+
+    function showNextGame($selector, runThisManyTimes){
+        if (runThisManyTimes == 0){
+            // show the game to play after brief delay
+            setTimeout(function(){$selector.fadeToggle(5000)}, 1000);
+            return;
+        }
+
+        $selector.fadeToggle(100, function(){
+            $(this).fadeToggle(100);
+            if ($(this).next().length > 0){
+                showNextGame($(this).next(), runThisManyTimes - 1);
+            }
+            else {
+                showNextGame($('.game').first(), runThisManyTimes - 1);
+            }
+        });
+    }
 })();
